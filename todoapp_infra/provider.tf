c40bdfc7ae000b2586db6fc27dd9606cbf415459
ajaysingh484 <ajays1268@gmail.com>
2025-08-15 23:11:18 +0530

    Update provider.tf

--- todoapp_infra/provider.tf
+++ todoapp_infra/provider.tf
@@ -1,5 +1,9 @@
 
 terraform {
+  backend "azurerm" {
+    storage_account_name = "storageccountworkload"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "ajayneelam.tfstate"                # Can be passed via 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
