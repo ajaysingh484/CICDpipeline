bfd77daaa118709f53a719a89ec509d2d3c16c68
as4118 <ajay.singh@Zebra.com>
2025-09-01 14:41:01 +0530

    new code

--- todoapp_infra/provider.tf
+++ todoapp_infra/provider.tf
@@ -1,18 +1,19 @@
 
 terraform {
-  backend "azurerm" {
-    storage_account_name = "storageccountworkload"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "ajayneelam.tfstate"                # Can be passed via 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.32.0"
-    }
+  {
+    storage_account_name = "storageccountworkload"
+    container_name       = "tfstate"                                
+    key                  = "ajayneelam.tfstate"               
   }
 }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.42.0"
+    }
+  }
 
 provider "azurerm" {
   features {}
-  subscription_id = "7ccb66b8-87a3-4a6a-9f86-aa9aaad3bf02"
+  subscription_id ="7ccb66b8-87a3-4a6a-9f86-aa9aaad3bf02"
 }
